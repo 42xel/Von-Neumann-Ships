7027a92b92536895c249e885e01a8a510acbc61a
Aurélien Emmanuel <a.emmanuel@nanocorp.fr>
2024-07-13 15:56:36 +0200

    changed representation, fixed SIGSEGV bugs

--- src/ternary_logic.c
+++ src/ternary_logic.c
@@ -5,7 +5,8 @@
 
 unsigned short _TRIT[256];
 const unsigned short* TRIT = _TRIT + 128;
-const signed char BIN[1>>10];
+signed char _BIN[1 << 10];
+const signed char* BIN = _BIN;
 static char _init = 0;
 
             fn signed char from_ltrit
@@ -29,7 +30,7 @@ l <<= 2;
 unsigned short hl = 0;
     if (n < 0) return
 hl = to_ltrit(-n),
-~(((hl & 0x155) << 1) | ((hl & 0x2AA) >> 1));
+(((hl & 0x155) << 1) | ((hl & 0x2AA) >> 1)) ^ 0x03FF;
         for (int i = 0; i < 5; ++i) {
 hl >>= 2;
 ++n;
@@ -55,14 +56,13 @@ err += (i != from_ltrit(to_ltrit(i))); }
     if (_init) return;
 unsigned short* trit = TRIT;
 signed char* bin = BIN;
-        for (signed char i = 1; i++ != 0;)//is overflow UB?
-{printf("trit: %x,\ti: %x,\t &trit[i]: %x\n", trit, i, (trit) + (i));
+bin[2] = 4;
+        signed char i = 0; do
 trit[i] = 0xFEAA; // recognizable impossible state
-}
-        for (unsigned short j = -1 << 10; j++ != 0;)
-            {printf("bin: %x,\tj: %x,\t &trit[i]: %x\n", trit, i, (trit) + (i));
+while (++i != 0);
+        unsigned short j; do
 bin[j] = 0x80; // recognizable impossible state
-}
+while (++j < 1 << 10);
         for (signed char i = -121; i <= +121; ++i) {
 unsigned short t = to_ltrit(i);
 trit[i] = t;
@@ -80,10 +80,9 @@ err += (trit[i] ^= 0xFEAA);
         for (signed char i = -121; i <= +121; ++i)
 err += (i != BIN[trit[i]]);
     return err; }
-            /*
             fn char test3() {
 init_tables();
-unsigned short* bin = BIN;
+signed char* bin = BIN;
 char err = 0;
         for (unsigned short j = 0; j < 1 << 10; ++j) {
     if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
@@ -92,12 +91,12 @@ continue; }
     err += (j != TRIT[bin[j]]);
 }
     return err; }
-    */
+
             char main () {
 char r;
 init_tables();
 return
 (r = test1()) ? r :
 (r = test2()) ? r :
-// (r = test3()) ? r :
+(r = test3()) ? r :
 0; }
