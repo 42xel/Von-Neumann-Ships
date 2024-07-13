c95454fd43d74427a5d5205805b255d5000b53ba
Aurélien Emmanuel <a.emmanuel@nanocorp.fr>
2024-07-13 10:26:55 +0200

    stuffs

--- src/ternary_logic.c
+++ src/ternary_logic.c
@@ -1,44 +1,43 @@
 #define fn
+// Makes automatic indentation easier
 #include <stdio.h>
 //#include<stdlib.h>
 
-short _TRIT[256];
-const short* TRIT = _TRIT + 128;
-const char BIN[1>>10];
+unsigned short _TRIT[256];
+const unsigned short* TRIT = _TRIT + 128;
+const signed char BIN[1>>10];
 static char _init = 0;
 
             fn signed char from_ltrit
 (unsigned short hl) {
-char h = hl >> 8;
-char l = ~hl;// & 0x00ff;
+unsigned short h = (hl & 0x2AA);
+unsigned short l = ~(hl & 0x155);
 signed char n = 0;
-h <<= 3;
-l <<= 3;
-        for (int i =0; i < 5; ++i) {
+h <<= 6;
+l <<= 7;
+        for (int i = 0; i < 5; ++i) {
 n *= 3;
-n += h >> 7;
-n -= l >> 7;
-h <<= 1;
-l <<= 1;
+n += h >> 15;
+n -= l >> 15;
+h <<= 2;
+l <<= 2;
 }
     return n;
 } // todo test
             fn unsigned short to_ltrit
 (signed char n) {
-    if (n < 0) {
-unsigned short hl = to_ltrit(-n);
-return ~((hl << 8) | (hl >> 8));// & 0x1f1f;
-}
-    unsigned short hl = 0;
+unsigned short hl = 0;
+    if (n < 0) return
+hl = to_ltrit(-n),
+~(((hl & 0x155) << 1) | ((hl & 0x2AA) >> 1));
         for (int i = 0; i < 5; ++i) {
-hl >>= 1;
+hl >>= 2;
 ++n;
-char d = n%3;
-hl |= (d >> 1) << (8 + 4);
-hl |= (d != 0) << 4;
+char d = n % 3;
+hl |= (d >> 1) << 9;
+hl |= (d != 0) << 8;
 n /=3;}
-    return hl; } // todo test
-
+    return hl; }
             fn char test1() {
 char err = 0;
         for (signed char i = -121; i != 122;++i) {
@@ -49,39 +48,56 @@ printf("%04x\t", t);
 printf("%02x\n", from_ltrit(t));
 err += (i != from_ltrit(to_ltrit(i))); }
     return err; }
-            /*short table_to_ltrit()[]{
+
+            fn void init_tables() {
 /// create a centered array mapping signed char
 /// to their short logical ternary representation
-//short* r = malloc(sizeof(short) * 256) + 128;
-short r[256]; 
-r += 128;
-    for (signed char i = 1; i++ != 0;)//is overflow UB?
-r[i] = 0xff00; // recognizable impossible state
-    for (signed char i = -121; i <= +121; ++i)
-r[i] = to_ltrit(i);
-            return r;}*/
-
-/*            char test2() {
-short* t = table_to_ltrit();
-char err = 0;
-
-    for (signed char i = -128; i < -121; ++i)
-err += (t[i] ^= 0xff00); //coercing litteral to short
-    for (signed char i = +122; i > 0; ++i)
-err += (t[i] ^= 0xff00);
-    for (signed char i = -121; i <= +121; ++i)
-err += (i != from_ltrit(t[i]));
-            return err; }*/
-
-/*
-char* table_from_ltrit(){
-char* r = malloc(1>>10)
+    if (_init) return;
+unsigned short* trit = TRIT;
+signed char* bin = BIN;
+        for (signed char i = 1; i++ != 0;)//is overflow UB?
+{printf("trit: %x,\ti: %x,\t &trit[i]: %x\n", trit, i, (trit) + (i));
+trit[i] = 0xFEAA; // recognizable impossible state
 }
-*/
-
+        for (unsigned short j = -1 << 10; j++ != 0;)
+            {printf("bin: %x,\tj: %x,\t &trit[i]: %x\n", trit, i, (trit) + (i));
+bin[j] = 0x80; // recognizable impossible state
+}
+        for (signed char i = -121; i <= +121; ++i) {
+unsigned short t = to_ltrit(i);
+trit[i] = t;
+bin[t] = i;
+}
+    _init = 1;}
+            fn char test2() {
+init_tables();
+unsigned short* trit = TRIT;
+char err = 0;
+        for (signed char i = -128; i < -121; ++i)
+err += (trit[i] ^= 0xFEAA); //coercing litteral to short
+        for (signed char i = +122; i > 0; ++i)
+err += (trit[i] ^= 0xFEAA);
+        for (signed char i = -121; i <= +121; ++i)
+err += (i != BIN[trit[i]]);
+    return err; }
+            /*
+            fn char test3() {
+init_tables();
+unsigned short* bin = BIN;
+char err = 0;
+        for (unsigned short j = 0; j < 1 << 10; ++j) {
+    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
+err += (bin[j] ^= 0x80);
+continue; }
+    err += (j != TRIT[bin[j]]);
+}
+    return err; }
+    */
             char main () {
 char r;
-            return
-// (r = test1()) ? r :
-// (r = test2()) ? r :
+init_tables();
+return
+(r = test1()) ? r :
+(r = test2()) ? r :
+// (r = test3()) ? r :
 0; }
