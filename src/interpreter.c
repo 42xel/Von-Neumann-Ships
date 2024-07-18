ce0f0ebacb92e319d967d8be601c6cdca15be962
Aurélien Emmanuel <a.emmanuel@nanocorp.fr>
2024-07-18 17:21:10 +0200

    ternary arith

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,44 +1,31 @@
-#include <stdio.h>
+#include "prelude.h"
+#include "ternary_logic.h"
 
-            signed char from_ltrit(unsigned short hl) {
-char h = hl >> 8;
-char l = ~hl;// & 0x00ff;
-signed char n = 0;
-h <<= 3;
-l <<= 3;
-    for (int i =0; i < 5; ++i) {
-n *= 3;
-n += h >> 7;
-n -= l >> 7;
-    h <<= 1;
-    l <<= 1;
-    }
-            return n;} // todo test
+// for now, registers are at a fixed address:
+// *0 for p
+// -*0 for c
+// *0iiii for read ; defaults to i0000 ?
+// *01111 for write : defaluts to 10000 ?
+// there is one normal clock at 0
 
-            unsigned short to_ltrit(signed char n) {
-    if (n < 0) {
-unsigned short hl = to_ltrit(-n);
-return ~((hl << 8) | (hl >> 8));// & 0x1f1f;
+signed char PROG = 0;
+signed char READ = -40;
+signed char WRIT = 40;
+
+            fn result step (signed char tape[243]) {
+unsigned short operation = TRIT[tape[121 + PROG]] << 6;
+signed char constant = tape[121 - PROG];
+signed char read = tape[121 + read];
+signed char writ = tape[121 + writ];
+
+char rwc = operation >> 14;
+operation <<=2;
+
 }
-unsigned short hl = 0;
-    for (int i = 0; i < 5; ++i) {
-hl >>= 1;
-++n;
-char d = n%3;
-hl |= (d >> 1) << (8 + 4);
-hl |= (d != 0) << 4;
-    n /=3;}
-            return hl; } // todo test
 
-            int test1() {
-char err = 0;
-    for (signed char j = -121; j != 122;++j) {
-unsigned short t = to_ltrit(j);
-printf("%02x\t", j);
-printf("%04x\n", t);
+            fn result main() {
+signed char tape[243];
+// todo load data from binary
+    //initialising the registers
 
-printf("%04x\t", t);
-printf("%02x\n", from_ltrit(t));
-if (j != from_ltrit(to_ltrit(j))) ++err; }
-            return err; }
-
+return 0; }
--- src/ternary_logic.c
+++ src/ternary_logic.c
@@ -1,10 +1,45 @@
 #include "prelude.h"
 
-unsigned short _TRIT[256];
-const unsigned short* TRIT = _TRIT + 128;
-signed char _BIN[1 << 10];
-const signed char* BIN = _BIN;
+// todo display ?
+// addition ?
+// operations ?
+
+unsigned short _LTRIT[256];
+const unsigned short* LTRIT = _LTRIT + 128;
+signed char _L2BIN[1 << 10];
+const signed char* L2BIN = _L2BIN;
+unsigned short _ATRIT[256];
+const unsigned short* ATRIT = _ATRIT + 128;
+signed char _A2BIN[1 << 10];
+const signed char* A2BIN = _A2BIN;
 static char _init = 0;
+
+            fn signed char from_atrit
+(signed short hl) {
+hl <<= 6;
+signed char n = 0;
+        for (int _ = 0; _++ < 5; hl <<= 2) {
+n *= 3;
+n += hl >> 14;
+}
+    return n; }
+            fn signed short to_atrit
+(signed char n) {
+unsigned short hl = 0;
+    if (n < 0) return
+hl = to_atrit(-n),
+hl ^ ((hl & 0x155) << 1);
+        for (int _ = 0; _++ < 5; n /= 3) {
+hl >>= 2;
+hl |= ((++n) % 3 - 1) << 14;
+}
+    return hl >> 6; }
+            fn result test_atrit() {
+result err = 0;
+        for (signed char i = -121; i != 122;++i) {
+unsigned short t = to_atrit(i);
+err += (i != from_atrit(to_atrit(i))); }
+    return err; }
 
             fn signed char from_ltrit
 (unsigned short hl) {
@@ -13,78 +48,107 @@ unsigned short l = ~(hl & 0x155);
 signed char n = 0;
 h <<= 6;
 l <<= 7;
-        for (int i = 0; i < 5; ++i) {
-n *= 3;
-n += h >> 15;
+        for (int _ = 0; _++ < 5; h <<= 2, l <<= 2)
+n *= 3,
+n += h >> 15,
 n -= l >> 15;
-h <<= 2;
-l <<= 2;
-}
-    return n;
-} // todo test
+    return n; }
             fn unsigned short to_ltrit
 (signed char n) {
 unsigned short hl = 0;
     if (n < 0) return
 hl = to_ltrit(-n),
 (((hl & 0x155) << 1) | ((hl & 0x2AA) >> 1)) ^ 0x03FF;
-        for (int i = 0; i < 5; ++i) {
+        for (int _ = 0; _++ < 5; n /=3) {
 hl >>= 2;
 ++n;
 char d = n % 3;
 hl |= (d >> 1) << 9;
 hl |= (d != 0) << 8;
-n /=3;}
+}
     return hl; }
-            fn result test1() {
-char err = 0;
+            fn result test_ltrit() {
+result err = 0;
         for (signed char i = -121; i != 122;++i) {
 unsigned short t = to_ltrit(i);
-printf("%02x\t", i);
-printf("%04x\n", t);
-printf("%04x\t", t);
-printf("%02x\n", from_ltrit(t));
 err += (i != from_ltrit(to_ltrit(i))); }
     return err; }
 
-            fn void init_tables() {
+            fn unsigned short arith_log (unsigned short a) {
+unsigned short al = a | ~0x155;
+signed short ah = a | ~0x2AA;
+unsigned short r = (ah ^ ((al << 1) | 1)) | (~ah >> 1);
+return r;
+}
+
 /// create a centered array mapping signed char
 /// to their short logical ternary representation
+            fn void init_tables() {
     if (_init) return;
-unsigned short* trit = TRIT;
-signed char* bin = BIN;
+unsigned short* ltrit = LTRIT;
+unsigned short* atrit = ATRIT;
+signed char* lbin = L2BIN;
+signed char* abin = A2BIN;
         signed char i = 0; do
-trit[i] = 0xFEAA; // recognizable impossible state
+atrit[i] = 0xFEAA, // recognizable impossible state
+ltrit[i] = 0xFEAA;
 while (++i != 0);
         unsigned short j; do
-bin[j] = 0x80; // recognizable impossible state
+abin[j] = 0x80, // recognizable impossible state
+lbin[j] = 0x80;
 while (++j < 1 << 10);
         for (signed char i = -121; i <= +121; ++i) {
-unsigned short t = to_ltrit(i);
-trit[i] = t;
-bin[t] = i;
+unsigned short a = to_atrit(i);
+atrit[i] = a;
+abin[a] = i;
+unsigned short l = arith_log(a);
+ltrit[i] = l;
+lbin[l] = i;
 }
     _init = 1;}
-            fn result test2() {
+
+            fn result test_LTRIT() {
 init_tables();
-unsigned short* trit = TRIT;
-char err = 0;
+unsigned short* trit = LTRIT;
+result err = 0;
         for (signed char i = -128; i < -121; ++i)
 err += (trit[i] ^= 0xFEAA); //coercing litteral to short
         for (signed char i = +122; i > 0; ++i)
 err += (trit[i] ^= 0xFEAA);
         for (signed char i = -121; i <= +121; ++i)
-err += (i != BIN[trit[i]]);
+err += (i != L2BIN[trit[i]]);
     return err; }
-            fn char test3() {
+            fn result test_L2BIN() {
 init_tables();
-signed char* bin = BIN;
-char err = 0;
+signed char* bin = L2BIN;
+result err = 0;
         for (unsigned short j = 0; j < 1 << 10; ++j) {
     if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
 err += (bin[j] ^= 0x80);
 continue; }
-    err += (j != TRIT[bin[j]]);
+    err += (j != LTRIT[bin[j]]);
+}
+    return err; }
+            fn result test_ATRIT() {
+init_tables();
+unsigned short* trit = ATRIT;
+result err = 0;
+        for (signed char i = -128; i < -121; ++i)
+err += (trit[i] ^= 0xFEAA); //coercing litteral to short
+        for (signed char i = +122; i > 0; ++i)
+err += (trit[i] ^= 0xFEAA);
+        for (signed char i = -0; i <= +1; ++i)
+err += (i != A2BIN[trit[i]]);
+    return err; }
+            fn result test_A2BIN() {
+init_tables();
+signed char* bin = A2BIN;
+result err = 0;
+        for (unsigned short j = 0; j < 1 << 10; ++j) {
+    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
+err += (bin[j] ^= 0x80);
+continue; }
+    err += (j != ATRIT[bin[j]]);
 }
     return err; }
 
@@ -92,7 +156,10 @@ continue; }
 char r;
 init_tables();
 return
-(r = test1()) ? r :
-(r = test2()) ? r :
-(r = test3()) ? r :
+(r = printf("test_atrit:\n"), test_atrit()) ? r :
+(r = printf("test_ltrit:\n"), test_ltrit()) ? r :
+(r = printf("test_ATRIT:\n"), test_ATRIT()) ? r :
+(r = printf("test_A2BIN:\n"), test_A2BIN()) ? r :
+(r = printf("test_LTRIT:\n"), test_LTRIT()) ? r :
+(r = printf("test_L2BIN:\n"), test_L2BIN()) ? r :
 0; }
