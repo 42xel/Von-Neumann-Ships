d5137f42131b63d4ff70442c27248b07d0686007
Aurélien Emmanuel <a.emmanuel@nanocorp.fr>
2024-07-25 03:23:17 +0200

    Added display (printf formats) to ternary logic

--- src/ternary_logic.c
+++ src/ternary_logic.c
@@ -1,17 +1,22 @@
 #include "prelude.h"
+#include "ternary_logic.h"
+
+#include <stdarg.h>
+// #include <string.h>
+#include <printf.h>
 
 // todo display ?
 // addition ?
 // operations ?
 
 unsigned short _LTRIT[256];
-const unsigned short* LTRIT = _LTRIT + 128;
+static const unsigned short* LTRIT = _LTRIT + 128;
 signed char _L2BIN[1 << 10];
-const signed char* L2BIN = _L2BIN;
+static const signed char* L2BIN = _L2BIN;
 unsigned short _ATRIT[256];
-const unsigned short* ATRIT = _ATRIT + 128;
+static const unsigned short* ATRIT = _ATRIT + 128;
 signed char _A2BIN[1 << 10];
-const signed char* A2BIN = _A2BIN;
+static const signed char* A2BIN = _A2BIN;
 static char _init = 0;
 
             fn signed char from_atrit
@@ -81,9 +86,103 @@ unsigned short r = (ah ^ ((al << 1) | 1)) | (~ah >> 1);
 return r;
 }
 
+            fn result test_LTRIT() {
+init();
+unsigned short* trit = LTRIT;
+result err = 0;
+        for (signed char i = -128; i < -121; ++i)
+err += (trit[i] ^= 0xFEAA); //coercing litteral to short
+        for (signed char i = +122; i > 0; ++i)
+err += (trit[i] ^= 0xFEAA);
+        for (signed char i = -121; i <= +121; ++i)
+err += (i != L2BIN[trit[i]]);
+    return err; }
+            fn result test_L2BIN() {
+init();
+signed char* bin = L2BIN;
+result err = 0;
+        for (unsigned short j = 0; j < 1 << 10; ++j) {
+    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
+err += (bin[j] ^= 0x80);
+continue; }
+    err += (j != LTRIT[bin[j]]);
+}
+    return err; }
+            fn result test_ATRIT() {
+init();
+unsigned short* trit = ATRIT;
+result err = 0;
+        for (signed char i = -128; i < -121; ++i)
+err += (trit[i] ^= 0xFEAA); //coercing litteral to short
+        for (signed char i = +122; i > 0; ++i)
+err += (trit[i] ^= 0xFEAA);
+        for (signed char i = -0; i <= +1; ++i)
+err += (i != A2BIN[trit[i]]);
+    return err; }
+            fn result test_A2BIN() {
+init();
+signed char* bin = A2BIN;
+result err = 0;
+        for (unsigned short j = 0; j < 1 << 10; ++j) {
+    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
+err += (bin[j] ^= 0x80);
+continue; }
+    err += (j != ATRIT[bin[j]]);
+}
+    return err; }
+
+            /// Custom printf handler for arithmetic trits
+            fn int atrit_handler
+(FILE *stream, const struct printf_info *info, const void *const *args) {
+signed char value = *((signed char *)args[0]);
+signed short a = to_atrit(value) << 6;
+char buffer[6];
+int index = 0;
+        for (int _ = 0; _++ < 5; a <<= 2) {
+    switch (a >> 14) {
+case +1: buffer[index++] = 'l'; break;
+case 00: buffer[index++] = 'o'; break;
+case -1: buffer[index++] = 'i'; break;
+default: buffer[index++] = '?'; break;
+}}
+    buffer[index] = '\0';
+return fprintf(stream, "%s", buffer); }
+            /// Argument size function
+            fn int atrit_arginfo
+(const struct printf_info *info, size_t n, int *argtypes) {
+if (n > 0) argtypes[0] = PA_INT;
+return 1; }
+
+            // Custom handler for logical trits
+            fn int ltrit_handler
+(FILE *stream, const struct printf_info *info, const void *const *args) {
+signed char value = *((signed char *)args[0]);
+unsigned short l = to_ltrit(value) << 6;
+char buffer[6];
+int index = 0;
+        for (int _ = 0; _++ < 5; l <<= 2) {
+    switch (l >> 14) {
+case 0b01: buffer[index++] = 'm'; break;
+case 0b00: buffer[index++] = 'n'; break;
+case 0b11: buffer[index++] = 'u'; break;
+default:   buffer[index++] = '?'; break;
+}}
+    buffer[index] = '\0';
+return fprintf(stream, "%s", buffer); }
+            fn int ltrit_arginfo
+(const struct printf_info *info, size_t n, int *argtypes) {
+if (n > 0) argtypes[0] = PA_INT;
+return 1; }
+
+                    fn result test_printf() {
+signed char test_value = 42;
+printf("Arithmetic Trits: %O\n", test_value);   // liiio
+printf("Logical Trits: %M\n", test_value);
+return 0; }
+
 /// create a centered array mapping signed char
 /// to their short logical ternary representation
-            fn void init_tables() {
+            fn void init() {
     if (_init) return;
 unsigned short* ltrit = LTRIT;
 unsigned short* atrit = ATRIT;
@@ -105,56 +204,13 @@ unsigned short l = arith_log(a);
 ltrit[i] = l;
 lbin[l] = i;
 }
+register_printf_function('O', atrit_handler, atrit_arginfo);
+register_printf_function('M', ltrit_handler, ltrit_arginfo);
     _init = 1;}
-
-            fn result test_LTRIT() {
-init_tables();
-unsigned short* trit = LTRIT;
-result err = 0;
-        for (signed char i = -128; i < -121; ++i)
-err += (trit[i] ^= 0xFEAA); //coercing litteral to short
-        for (signed char i = +122; i > 0; ++i)
-err += (trit[i] ^= 0xFEAA);
-        for (signed char i = -121; i <= +121; ++i)
-err += (i != L2BIN[trit[i]]);
-    return err; }
-            fn result test_L2BIN() {
-init_tables();
-signed char* bin = L2BIN;
-result err = 0;
-        for (unsigned short j = 0; j < 1 << 10; ++j) {
-    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
-err += (bin[j] ^= 0x80);
-continue; }
-    err += (j != LTRIT[bin[j]]);
-}
-    return err; }
-            fn result test_ATRIT() {
-init_tables();
-unsigned short* trit = ATRIT;
-result err = 0;
-        for (signed char i = -128; i < -121; ++i)
-err += (trit[i] ^= 0xFEAA); //coercing litteral to short
-        for (signed char i = +122; i > 0; ++i)
-err += (trit[i] ^= 0xFEAA);
-        for (signed char i = -0; i <= +1; ++i)
-err += (i != A2BIN[trit[i]]);
-    return err; }
-            fn result test_A2BIN() {
-init_tables();
-signed char* bin = A2BIN;
-result err = 0;
-        for (unsigned short j = 0; j < 1 << 10; ++j) {
-    if ( (j & 0x2AA) & (~(j & 0x155) << 1)) {
-err += (bin[j] ^= 0x80);
-continue; }
-    err += (j != ATRIT[bin[j]]);
-}
-    return err; }
 
             fn result main () {
 char r;
-init_tables();
+init();
 return
 (r = printf("test_atrit:\n"), test_atrit()) ? r :
 (r = printf("test_ltrit:\n"), test_ltrit()) ? r :
@@ -162,4 +218,5 @@ return
 (r = printf("test_A2BIN:\n"), test_A2BIN()) ? r :
 (r = printf("test_LTRIT:\n"), test_LTRIT()) ? r :
 (r = printf("test_L2BIN:\n"), test_L2BIN()) ? r :
+(r = printf("test_printf:\n"), test_printf()) ? r :
 0; }
--- src/ternary_logic.h
+++ src/ternary_logic.h
@@ -1,5 +1,84 @@
-const unsigned short* TRIT;
-const signed char* BIN;
-void init_tables();
-signed char from_ltrit (unsigned short hl);
+// A module to deal with ternary logic:
+// converting from byte (eight bits, 2^8)
+// to five trits (3^5) and back;
+// displaying such units group;
+// maybe other things;
+
+#ifndef ternary_logic
+#define ternary_logic
+
+/// The conversion functions aren't meant to be used,
+/// instead the following tables should be used.
+static const unsigned short* LTRIT;
+static const signed char* L2BIN;
+static const unsigned short* ATRIT;
+static const signed char* A2BIN;
+/// Initialise the tables above, and register custom printf functions.
+/// Calling it a secnd time does nothing.
+void init();
+
+/**
+Converts a signed byte to 5 arithmetical trits,
+Provided the values are within [-121, +121].
+        # Arithmetic Trits.
+The system used is base 3, but with the symbol values -1, 0 and 1.
+    ## Representation
+In the emulator, the values are stored as 2 bits, 2 complements numbers.
+thusly:
+* -1 is represented by 11
+*  0 is represented by 00
+* +1 is represented by 01
+* 10 corresponds to nothing
+The five double bits are stored in the least significant bits
+of a signed short.
+Arithmetic shift allows to retrieve the values -1, 0 and +1.
+    ## Display
+The symbols used for display are
+* i for -1
+* o for  0
+* l for +1
+Numbers from 0 to +5 :
+o, l, li, lo, ll, lii
+Minus 6 :
+ilo
+*/
+signed short to_atrit (signed char n);
+/// Convert from arithmetic trits to byte.
+/// See `to_atrit` for an explanation on arith trits.
+signed char from_atrit (signed short a);
+/**
+Converts a signed byte to logical trits.
+        # Logical Trits.
+They're arithmetical trits, but -1 means false, 1 true and 0 maybe.
+    ## Representation
+In the emulator, the values are stored as increasing 2 bits among
+00, 01, 11. Increasing in the sense false < maybe < true
+thusly:
+* -1 is represented by 11
+*  0 is represented by 00
+* +1 is represented by 01
+* 10 corresponds to nothing
+An other way to view it is as regular ternary with values 0 1 and 2
+represented in unary. Values in [0, 242] are encoded and are mapped
+increasingly to signed bytes of values [-121, +121].
+Either way, the five double bits are stored in the least significant bits
+of an unsigned short.
+Logical shift allows to retrieve the values 00, 01 and 11.
+    ## Display
+The symobls used for display are
+* n for 00 (*n*ot, and is mirror of u)
+* m for 01 (*m*aybe)
+* u for 11 (tru*e*)
+Some numbers :
+nmumu, nunu
+Numbers are preferably displayed of length 5, if some are ommited, assume
+leading `m`s.
+*/
 unsigned short to_ltrit (signed char n);
+/// Convert from logical trits to byte.
+/// See `to_ltrit` for an explanation on logical trits.
+signed char from_ltrit (unsigned short l);
+/// Convert from arithmetic to logical.
+unsigned short arith_log (unsigned short a);
+
+#endif
